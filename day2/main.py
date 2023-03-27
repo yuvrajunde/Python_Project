9aa8a6b58ce063309e562fbddfa46a983ee78301
yuvrajunde <yuvrajunde80@gmail.com>
2023-03-27 23:10:01 +0530

    added day2 code and document

---
+++ day2/main.py
@@ -0,0 +1,27 @@
+# password = input("enter a passwword:")
+# while password != "india":
+#     password = input("Remember it and type")
+# print("well done!")
+
+# number = int(input("Enter a numnber:"))
+# i = 1
+# while number > i:
+#     print(i)
+#     i = i + 1
+# Create a program that prompts the user to input their name once. Then, the program prints out the name once with the first letter capitalized.
+
+# user_input = input("Enter a user name")
+# print(user_input.capitalize())
+
+#Create a program that prompts the user to input their name once. Then, the program repeatedly prints out the name with the first letter capitalized.
+
+# user_input = input("Enter a name:")
+# while True:
+#     print(user_input.capitalize())
+
+#Create a program that prompts the user to input their name repeatedly. Then, the program repeatedly prints out the name with the first letter capitalized.
+user_input = input("Enter a name:")
+while True:
+    user = input("enter a name:")
+    print(user.capitalize())
+
