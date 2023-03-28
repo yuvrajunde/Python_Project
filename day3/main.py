d597af9612768ff337a26a222759f4393d0cc842
yuvrajunde <yuvrajunde80@gmail.com>
2023-03-29 00:37:09 +0530

    added day 3 changes

---
+++ day3/main.py
@@ -0,0 +1,45 @@
+# todos = []
+# while True:
+#      user_action = input("Type add, show or Exit:")
+#      match user_action:
+#          case 'add':
+#              todo = input("Enter a todo")
+#              todos.append(todo)
+#          case 'show' | 'display':
+#              print(todos)
+#              for item in todos:
+#                  print(item)
+#          case 'exit':
+#              break
+#          case whatever:
+#              print("Hey, you entered a wrong command")
+
+
+# user_input = input("enter a country name:")
+# while True:
+#
+#         match user_input:
+#             case 'USA':
+#                 print("hello")
+#                 break
+#
+#             case "India":
+#                 print("namaste")
+#             case "Germany":
+#                 print("Hallo")
+#             case _:
+#                 print("enter once again")
+#
+
+# ingredients = ["john smith", "sen plakay", "dora ngacely"]
+# for i in ingredients:
+#     print(i.title())
+
+for item in ["sandals", "glasses", "trousers"]:
+    print(item.capitalize())
+
+
+
+
+
+
